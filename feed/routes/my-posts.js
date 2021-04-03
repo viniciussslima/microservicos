197e7ae1b5fdc1883c6572c5232767a4d28df351
fagner dias <fagnerdiasmorais@gmail.com>
2021-04-03 16:38:05 -0300

    remoção de alguns console.log

--- feed/routes/my-posts.js
+++ feed/routes/my-posts.js
@@ -1,7 +1,8 @@
 var db = require("../utils/handlers/post");
 
-module.exports = (req, res, next) => {
-  db.findOne({ username: req.user.username }, (err, { posts }) => {
+module.exports = (req, res, next) => {  
+  console.log(req);
+  db.findOne({ username: req.user.username }, (err, { posts }) => {    
     res.send({
       posts,
     });
