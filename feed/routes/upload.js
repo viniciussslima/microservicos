6e36862bc03b028f9f3200ce0dcceb4e1f2fcd03
viniciussslima999 <vinicius.silva.017@ufrn.edu.br>
2021-04-05 12:28:11 -0300

    Ajuste para que os posts sejam salvos

--- feed/routes/upload.js
+++ feed/routes/upload.js
@@ -10,16 +10,16 @@ const { cloudinary, isSetup } = require("../config/cloudinary");
 module.exports = (req, res, next) => {
   // Generate a random id
   var random_id = guid.raw();
-  var final_location, type;  
-  if (req.files.filetoupload.name && isSetup) {    
+  var final_location, type;
+  if (req.files.filetoupload && req.files.filetoupload.name && isSetup) {
     cloudinary.v2.uploader.upload(
       req.files.filetoupload.path,
       function (error, result) {
         if (!error) {
           final_location = result.url;
           type = mime.lookup(req.files.filetoupload.name).split("/")[1];
-          dbPost.findOne({ username: req.user.username }, (err, u) => {            
-            if (u != undefined) {
+          dbPost.findOne({ username: req.user.username }, (err, u) => {
+            if (u) {
               u.posts.push({
                 _id: random_id,
                 author: req.user.username,
@@ -32,19 +32,45 @@ module.exports = (req, res, next) => {
                 type: type,
                 createdAt: new Date(),
                 lastEditedAt: new Date(),
-              });              
+              });
               u.save((err) => {
                 if (err) throw err;
                 res.status(204).send();
               });
             } else {
+              dbPost.createNew(
+                {
+                  username: req.user.username,
+                  posts: [
+                    {
+                      _id: random_id,
+                      author: req.user.username,
+                      authorID: p._id,
+                      static_url: final_location,
+                      caption: req.body.caption,
+                      category: req.body.type,
+                      comments: [],
+                      likes: [],
+                      type: type,
+                      createdAt: new Date(),
+                      lastEditedAt: new Date(),
+                    },
+                  ],
+                },
+                (err, p) => {
+                  p.save((err) => {
+                    if (err) throw err;
+                    res.status(204).send();
+                  });
+                }
+              );
               res.status(204).send();
             }
           });
         }
       }
     );
-  } else if (req.files.filetoupload.name) {
+  } else if (req.files.filetoupload && req.files.filetoupload.name) {
     var oldpath = req.files.filetoupload.path;
     var newpath = path.join(
       __dirname,
@@ -53,29 +79,53 @@ module.exports = (req, res, next) => {
     var final_location = `/feeds/${req.user._id}_${random_id}${req.files.filetoupload.name}`;
     var type = mime.lookup(req.files.filetoupload.name).split("/")[1];
     mv(oldpath, newpath, function (err) {});
-      dbPost.findOne({username: req.user.username}, (err, p) =>{        
-        if(p){
-          p.posts.push({
-            _id: random_id,
-            author: req.user.username,
-            authorID: p._id,
-            static_url: final_location,
-            caption: req.body.caption,
-            category: req.body.type,
-            comments: [],
-            likes: [],
-            type: type,
-            createdAt: new Date(),
-            lastEditedAt: new Date(),
-          });
-        } else{
-          dbPost.createNew({username: req.user.username}, (err, p));          
-        }
+    dbPost.findOne({ username: req.user.username }, (err, p) => {
+      if (p) {
+        p.posts.push({
+          _id: random_id,
+          author: req.user.username,
+          authorID: p._id,
+          static_url: final_location,
+          caption: req.body.caption,
+          category: req.body.type,
+          comments: [],
+          likes: [],
+          type: type,
+          createdAt: new Date(),
+          lastEditedAt: new Date(),
+        });
         p.save((err) => {
           if (err) throw err;
           res.status(204).send();
         });
-      });                         
-          
+      } else {
+        dbPost.createNew(
+          {
+            username: req.user.username,
+            posts: [
+              {
+                _id: random_id,
+                author: req.user.username,
+                authorID: p._id,
+                static_url: final_location,
+                caption: req.body.caption,
+                category: req.body.type,
+                comments: [],
+                likes: [],
+                type: type,
+                createdAt: new Date(),
+                lastEditedAt: new Date(),
+              },
+            ],
+          },
+          (err, p) => {
+            p.save((err) => {
+              if (err) throw err;
+              res.status(204).send();
+            });
+          }
+        );
+      }
+    });
   }
 };
--- feed/utils/handlers/post.js
+++ feed/utils/handlers/post.js
@@ -9,81 +9,74 @@ mongoose.connect(require("../../config/app").db.connectionUri, {
   useNewUrlParser: true,
 });
 
+function createNew(obj, cb) {
+  Post.findOne({ username: obj.username }).exec((err, user) => {
+    if (user) {
+      return cb(null, false);
+    } else {
+      var newPost = new Post(obj);
+      newPost.save((err, res) => {
+        return cb(err, res);
+      });
+    }
+  });
+}
 
+function findOne(obj, cb) {
+  Post.findOne(obj).exec((err, post) => {
+    if (err) return cb(err, false);
+    if (post) {
+      return cb(err, post);
+    } else {
+      return cb(null, false);
+    }
+  });
+}
 
-function createNew(obj, cb) {    
-      Post.findOne({ username: obj.username }).exec((err, user) => {
-        if (user) {
-          return cb(null, false);
-        } else {          
-          var newPost = new Post({
-            username: obj.username,
-            post: []            
-          });          
-          newPost.save((err, res) => {
-            return cb(err, res);
-          });
-        }
-      });    
-  }
+function search(opt, cb) {
+  Post.find({ username: { $gt: opt } }).exec((err, results) => {
+    if (err) return cb(err, false);
+    if (results) {
+      return cb(err, results);
+    } else {
+      return cb(null, false);
+    }
+  });
+}
 
-  function findOne(obj, cb) {
-    Post.findOne(obj).exec((err, post) => {
-      if (err) return cb(err, false);
-      if (post) {
-        return cb(err, post);
-      } else {
-        return cb(null, false);
-      }
-    });
-  }
-
-
-  function search(opt, cb) {
-    Post.find({ username: { $gt: opt } }).exec((err, results) => {
-      if (err) return cb(err, false);
-      if (results) {
-        return cb(err, results);
-      } else {
-        return cb(null, false);
-      }
-    });
-  }
-  
-  /*****
+/*****
   usage:
      getAll((error, result) => {
           if (!result) return false;
           // Do something after...
       })
   *****/
-  
-  function getAll(cb) {
-    Post.find({}).exec((err, posts) => {
-      if (err) return cb(err, false);
-      if (posts) {          
-        return cb(null, posts);
-      } else {
-        return cb(null, false);
-      }
-    });
-  }
-  
-  function deleteOne(opt, cb) {
-    //if(typeof opt !== Object) cb("Must be a javascript object.");
-    Post.deleteOne(opt).exec((err, res) => {
-      if (err) return cb(err, null);
-      else if (res.n == 0) {
-        return cb(null, true);
-      }
-    });
-  }  
-  
-  // Expose all the api...
-  module.exports = { 
-    createNew: createNew,   
-    findOne: findOne,
-    getAll: getAll,    
-    search: search,
-  };
 
+function getAll(cb) {
+  Post.find({}).exec((err, posts) => {
+    if (err) return cb(err, false);
+    if (posts) {
+      return cb(null, posts);
+    } else {
+      return cb(null, false);
+    }
+  });
+}
+
+function deleteOne(opt, cb) {
+  //if(typeof opt !== Object) cb("Must be a javascript object.");
+  Post.deleteOne(opt).exec((err, res) => {
+    if (err) return cb(err, null);
+    else if (res.n == 0) {
+      return cb(null, true);
+    }
+  });
+}
+
+// Expose all the api...
+module.exports = {
+  createNew: createNew,
+  findOne: findOne,
+  getAll: getAll,
+  search: search,
+};
