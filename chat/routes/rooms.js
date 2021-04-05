34d3d0fa231e6c70c1369ada98bd6f3b68fea313
viniciussslima999 <vinicius.silva.017@ufrn.edu.br>
2021-04-05 11:52:34 -0300

    Removendo o proprio usuario da lista de usuarios

--- chat/routes/rooms.js
+++ chat/routes/rooms.js
@@ -2,6 +2,8 @@ var User = require("../models/user");
 
 module.exports = (req, res) => {
   User.find({}).exec((error, users) => {
+    let index = users.findIndex((user) => user.id === req.user.id);
+    users.splice(index, 1);
     res.status(200).send(users);
   });
 };
