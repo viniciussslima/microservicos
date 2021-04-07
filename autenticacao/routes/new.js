809cc10fc8afd17828cb9a5ac6e0d8b705049637
viniciussslima999 <vinicius.silva.017@ufrn.edu.br>
2021-04-07 15:14:49 -0300

    Adição do last login ao criar o usuario

--- autenticacao/routes/new.js
+++ autenticacao/routes/new.js
@@ -4,25 +4,29 @@ const db = require("../utils/user");
 const dbAuth = require("../utils/auth");
 
 module.exports = (req, res) => {
-  dbAuth.createNew(req.body, (err, resultAuth) =>{ 
-    db.createNew(req.body, (error, result) => {   
-    if(!resultAuth){
-      return res.status(400).send({
-        error: "Bad user details.",
-      });
-    } else if (!result) {
-      return res.status(400).send({
-        error: "Bad user details.",
-      });
-    } else {
-      let token = jwt.sign(
-        { username: req.body.username },
-        process.env.JWT_SECRET
-      );
-      res.status(200).send({
-        token,
-      });
-    }
-  });               
+  dbAuth.createNew(req.body, (err, resultAuth) => {
+    db.createNew(req.body, (error, result) => {
+      if (!resultAuth) {
+        return res.status(400).send({
+          error: "Bad user details.",
+        });
+      } else if (!result) {
+        return res.status(400).send({
+          error: "Bad user details.",
+        });
+      } else {
+        result.lastLogin = new Date();
+
+        result.save(() => {
+          let token = jwt.sign(
+            { username: req.body.username },
+            process.env.JWT_SECRET
+          );
+          res.status(200).send({
+            token,
+          });
+        });
+      }
+    });
   });
 };
